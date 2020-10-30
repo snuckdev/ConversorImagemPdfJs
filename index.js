2965ac5ba56290609a66f9d82ee1c8511d2f034e
snuckdev <joseeduardoampla@gmail.com>
2020-10-30 14:23:58 -0300

    Criação do diretório automática

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const criarPdf = require('./converterImagem')
 const yargs = require('yargs')
+const fs = require('fs')
+const criarPdf = require('./converterImagem')
 
 let argv = yargs.argv
 
 let diretorio_imagens = argv.dir
 let arquivo_destino = argv.out
 let senha = argv.password
+
+let nome_diretorio = './imagens'
+
+if(!fs.existsSync(nome_diretorio)) {
+  fs.mkdirSync(nome_diretorio)
+}
 
 criarPdf(diretorio_imagens, arquivo_destino, senha)
